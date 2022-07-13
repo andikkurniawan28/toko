bf238561ea1e82652d8b9b0331bf8bee449d18a8
refalde <109115544+refalde@users.noreply.github.com>
2022-07-13 17:13:10 +0700

    tes update header.php

--- application/views/static/header.php
+++ application/views/static/header.php
@@ -28,7 +28,7 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url();?>">
